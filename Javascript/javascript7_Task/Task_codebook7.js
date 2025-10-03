ec5f33c9c95aabf83eb935fa694623fa1ab2d783
Reuben <reubenmulero12@gmail.com>
2025-10-03 13:35:22 +0100

    m

--- Javascript/javascript7_Task/Task_codebook7.js
+++ Javascript/javascript7_Task/Task_codebook7.js
@@ -57,4 +57,81 @@ class Student {
 let newStudent = new Student("Ayo", 18, [40, 50, 60]);
 newStudent.getDetails();
 
+// Excercise 3
+class Vehicle {
+  constructor(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
 
+  getInfo() {
+    console.log("Brand: " + this.brand);
+    console.log("Model: " + this.model);
+    console.log("Year: " + this.year);
+  }
+
+  isOld() {
+    let now = new Date().getFullYear();
+    return now - this.year > 10;
+  }
+}
+
+class Car extends Vehicle {
+  constructor(brand, model, year, fuelType) {
+    super(brand, model, year);
+    this.fuelType = fuelType;
+  }
+
+  getInfo() {
+    super.getInfo();
+    console.log("Fuel Type: " + this.fuelType);
+  }
+}
+
+let Cr1 = new Car("Honda", "Civic", 2012, "Petrol");
+Cr1.getInfo();
+console.log("Is old? " + Cr1.isOld());
+
+// Excersie 4
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+class Cart {
+  constructor() {
+    this.items = [];
+  }
+
+  addProduct(product, quantity) {
+    this.items.push({ product: product, quantity: quantity });
+  }
+
+  removeProduct(name) {
+    this.items = this.items.filter(item => item.product.name !== name);
+  }
+
+  getTotal() {
+    let total = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      total += this.items[i].product.price * this.items[i].quantity;
+    }
+    if (total > 100) {
+      return "Free Shipping";
+    }
+    return "Total: N" + total;
+  }
+}
+let p1 = new Product("Bread", 40);
+let p2 = new Product("Milk", 70);
+
+let cart = new Cart();
+cart.addProduct(p1, 1); 
+cart.addProduct(p2, 1); 
+
+console.log(cart.getTotal()); 
+cart.removeProduct("Bread");
+console.log(cart.getTotal()); 
