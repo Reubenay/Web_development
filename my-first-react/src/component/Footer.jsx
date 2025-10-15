5e63a0e1ee9ba0e23792294e8df1730a6915e4ac
Reuben <reubenmulero12@gmail.com>
2025-10-15 15:40:46 +0100

    java commmit

---
+++ my-first-react/src/component/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer (){
+    const year = new Date().getFullYear();
+    return (
+        <footer>
+            <p> {year} Naija Tech Hub. All rights reserved.</p>
+           
+        </footer>
+    );
+}
+export default Footer;
